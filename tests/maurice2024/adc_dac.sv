4a718e740d8c403c1aad01d2b4303839e5636d58
Augusto Mafra <augustomafra1@gmail.com>
2025-08-31 20:19:43 -0300

    Fix adc_dac test case for floating point semantics

--- tests/maurice2024/adc_dac.sv
+++ tests/maurice2024/adc_dac.sv
@@ -4,7 +4,14 @@ module main;
     parameter n = 3;
     parameter real nlevels = $pow(2.0,n);
 
+    real nondet;
     real VIN, VOUT_A;
+
+    real positive;
+    assign positive = nondet >= 0.0 ? nondet : 0.0;
+    assign less_than_1 = positive <= 1.0 ? positive : 1.0;
+    assign VIN = less_than_1;
+
     reg [n-1:0] Q;
 
     flash_adc #(.n(n)) adc(VIN, VSUP, Q);
